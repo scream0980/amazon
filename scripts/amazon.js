b67a1d7086184187bff9f09e25940585f67aa358
Farhan Mohammed Khan <farhan22054cst@sdbc.ac.in>
2026-03-03 13:13:59 +0530

    Fix title typo and implement cart functionality with event listeners for adding products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,7 +44,8 @@ products.forEach((product)=> {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id = '${product.id}'>
             Add to Cart
           </button>
         </div>
@@ -54,3 +55,25 @@ products.forEach((product)=> {
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click',()=> {
+    const prodcutId = button.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item)=> {
+      if (prodcutId === item.prodcutId) {
+        matchingItem = item
+      }
+    })
+  
+    if(matchingItem){
+      matchingItem.quantity += 1
+    } else {
+      cart.push({
+      prodcutId : prodcutId,
+      quantity : 1
+    })
+    }
+    console.log(cart);
+  })
+})
